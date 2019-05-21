637d16f1c0c889c823d8846d7266473c3b3ff4e7
Jake <jakelovitz@gmail.com>
2019-05-21 09:22:27 -0400

    finished displaying associations lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #artist
+  get '/artists', to: 'artists#index'
+  get '/artists/new', to: 'artists#new'
+  post '/artists', to: 'artists#create'
+  get 'artists/:id', to: 'artists#show', as: '/artist'
+
+  #song
+  get '/songs', to: 'songs#index'
+  get '/songs/new', to: 'songs#new'
+  post '/songs', to: 'songs#create'
+  get 'songs/:id', to: 'songs#show', as: '/song'
 end
