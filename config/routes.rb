309732005c8f9e25846933fdecf1642d3fc63b98
Adam Sultanov <adam.sultanov@gmail.com>
2019-05-22 16:58:56 -0400

    this was a tuffy

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/artists', to: 'artists#index'
+  get '/artists/new', to: 'artists#new'
+  get '/artists/edit', to: 'artists#edit'
+  post '/artists', to: 'artists#create'
+  post '/artists/:id', to: 'artists#update'
+  get '/artists/:id', to: 'artists#show', as: 'artist'
+
+  get '/songs', to: 'songs#index'
+  get '/songs/new', to: 'songs#new'
+  get '/songs/edit', to: 'songs#edit'
+  post '/songs', to: 'songs#create'
+  post '/songs/:id', to: 'songs#update'
+  get '/songs/:id', to: 'songs#show', as: 'song'
 end
